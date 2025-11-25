f5f21603fe24268d7c1e229644bbf752985cc5b6
JCarmichael9 <526jcarmichael@frhsd.com>
2025-11-25 13:09:26 -0500

    List Rendering

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: 'A pair of warm, fuzzy socks.',
-            image: './assets/images/socks_green.jpg',
-            inventory: 100,
-            // solution
-            onSale: true
-            // solution
+            image: './assets/images/socks_blue.jpg',
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: ['S', 'M', 'L', 'XL'],
+            variants: [
+              { id: 2234, color: 'green' },
+              { id: 2235, color: 'blue' },
+            ]
         }
     }
-});
+})
