0f241113f7fd1eec0fe770c889063d71e2988760
JCarmichael9 <526jcarmichael@frhsd.com>
2025-11-25 13:04:45 -0500

    Attribute Binding

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      product: 'Socks',
-      description: 'Keeps feet warm'
-    };
-  }
-});
+    data() {
+        return {
+            product: 'Socks',
+            description: 'A pair of warm, fuzzy socks.',
+            image: './assets/images/socks_green.jpg',
+            // solution
+            url: 'https://www.vuemastery.com/'
+            // solution
+        }
+    }
+})
